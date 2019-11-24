9adf800830c7ef6004e6362c2e640f020ee29af0
Cesar Melo <cavmelo@gmail.com>
2019-11-24 15:05:53 -0400

    Initial commit

---
+++ credential.ts
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch'
+
+export class CredentialManager {
+    private _token: string | undefined
+
+    get token() { return this._token }
+    set token(token) { this._token = token}
+
+    public static login(email:string, password:string, url?:string|undefined): Promise<CredentialManager> {
+        var body = {
+            'email':email,
+            'password':password
+        }
+        return new Promise((resolve, reject) => {
+            fetch(url+'/users/authenticate', {
+
+                headers: { "Content-Type": "application/json; charset=utf-8" },
+                method: 'POST',
+                body: JSON.stringify(body)
+            })
+                .then(response=>response.json())
+                .then(json=>{
+                    console.log(json['accessToken'])
+                    const credential = new CredentialManager()
+                    credential._token=json['accessToken']
+                    resolve(credential)
+                })
+                .catch(error=>{
+                    reject(error)
+                })
+        })
+    }
+}
